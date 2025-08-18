4bcd5218c435a22d094bb313c456dc10b9edf4f0
Siphelele Malotana <164928472+Ririesphe@users.noreply.github.com>
2025-08-18 20:41:07 +0200

    Create  ReviewRepository.java

---
+++ src/main/java/za/ac/cput/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package za.ac.cput.repository;
+
+import  za.ac.cput.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+}
