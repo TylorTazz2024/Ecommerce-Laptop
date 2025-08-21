380ef3f74da0e6f5ab7f4434345f3fc121fa59ea
SinesiphoM <sinesiphomlawuli05@gmail.com>
2025-08-21 18:18:50 +0200

    order repository

--- src/main/java/za/ac/cput/repository/OrderRepository.java
+++ src/main/java/za/ac/cput/repository/OrderRepository.java
@@ -3,4 +3,31 @@ package za.ac.cput.repository;
 import java.util.*;
 
 public class OrderRepository {
+    private Map<Integer, Order> orderDB = new HashMap<>();
+
+    public Order save(Order order) {
+        orderDB.put(order.getOrderID(), order);
+        return order;
+    }
+
+    public Order findById(int orderID) {
+        return orderDB.get(orderID);
+    }
+
+    public List<Order> findAll() {
+        return new ArrayList<>(orderDB.values());
+    }
+
+    public Order update(Order order) {
+        if (orderDB.containsKey(order.getOrderID())) {
+            orderDB.put(order.getOrderID(), order);
+            return order;
+        }
+        return null;
+    }
+
+    public boolean delete(int orderID) {
+        return orderDB.remove(orderID) != null;
+    }
 }
+
