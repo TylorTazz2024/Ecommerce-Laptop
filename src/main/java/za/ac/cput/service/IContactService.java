8080e97d9229b048f6de25930aa066fc4c49479e
Mkhisto <khanyamankosi1@gmail.com>
2025-08-21 18:47:57 +0200

    IContactService

--- src/main/java/za/ac/cput/service/IContactService.java
+++ src/main/java/za/ac/cput/service/IContactService.java
@@ -1,13 +1,16 @@
 package za.ac.cput.service;
 
-import za.ac.cput.domain.Contact;
-
 import java.util.List;
 
-public interface IContactService {
+import za.ac.cput.domain.Contact;
+
+
+
+public interface IContactService
+                  {
     Contact create(Contact contact);
     Contact read(int id);
     Contact update(Contact contact);
     boolean delete(int id);
     List<Contact> getAllContacts();
-}
+      }
