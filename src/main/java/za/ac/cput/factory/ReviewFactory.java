f073129d2697cb6295ec63bdd92f2779bde39dd8
Siphelele Malotana <164928472+Ririesphe@users.noreply.github.com>
2025-08-21 22:51:12 +0200

    Update ReviewFactory.java

--- src/main/java/za/ac/cput/factory/ReviewFactory.java
+++ src/main/java/za/ac/cput/factory/ReviewFactory.java
@@ -5,12 +5,10 @@ import za.ac.cput.util.Helper;
 
 public class ReviewFactory {
 
-    public static Review buildReview(String rating, String 
+    public static Review buildReview(String rating, String comment) {
         if (!Helper.isValidRating(rating)) {
             throw new IllegalArgumentException("Invalid rating, must be between 1 and 5");
         }
-
-
         String cleanComment = Helper.sanitizeComment(comment);
         return new Review(rating, cleanComment);
     }
