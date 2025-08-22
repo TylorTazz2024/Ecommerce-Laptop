717cbb4ba644c9ac18b9e5ffdbafdcd9b116c0e9
Nontando Zondi <221248986@mycput.ac.za>
2025-08-22 10:47:59 +0200

    Laptop controller

--- src/main/java/za/ac/cput/service/IService.java
+++ src/main/java/za/ac/cput/service/IService.java
@@ -1,8 +1,12 @@
 package za.ac.cput.service;
 
+import java.util.List;
+
 public interface IService <T, ID> {
     T save (T t);
     T update (T t);
     T read (ID id);
     void delete (ID id);
+
+
 }
