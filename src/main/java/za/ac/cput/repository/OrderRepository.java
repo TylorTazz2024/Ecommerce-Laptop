e18b82bd57962202f5b83c489c5507a3a6c48ca8
SinesiphoM <sinesiphomlawuli05@gmail.com>
2025-08-25 18:31:20 +0200

    changes made on OrderRepository.java

---
+++ src/main/java/za/ac/cput/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package za.ac.cput.repository;
+
+import za.ac.cput.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+}
