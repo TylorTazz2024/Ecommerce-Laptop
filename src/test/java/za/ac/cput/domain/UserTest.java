8731075d49c6e8eb8dd7cd4c3c213fe9c5f4c986
TylorTazz2024 <165163298+TylorTazz2024@users.noreply.github.com>
2025-08-25 15:23:04 +0200

    UserTest

---
+++ src/test/java/za/ac/cput/domain/UserTest.java
@@ -0,0 +1,36 @@
+package za.ac.cput.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getUserID() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getPhone() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getRole() {
+    }
+}
