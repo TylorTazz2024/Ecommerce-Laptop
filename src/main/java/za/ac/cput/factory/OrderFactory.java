f8141c9d5afe523db21e718f107b958312c331bd
SinesiphoM <sinesiphomlawuli05@gmail.com>
2025-08-25 16:09:56 +0200

    changes made

--- src/main/java/za/ac/cput/factory/OrderFactory.java
+++ src/main/java/za/ac/cput/factory/OrderFactory.java
@@ -1,21 +1,23 @@
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.Order;
-
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class OrderFactory {
 
+    private static final AtomicInteger counter = new AtomicInteger(1000);
 
     public static Order createOrder(double totalAmount) {
+        int orderID = counter.incrementAndGet();
+
         return new Order.Builder()
                 .setOrderID(orderID)
-                .setOrderDate(new Date()) // Automatically set current date
-                .setStatus("Pending")    // Default status
+                .setOrderDate(new Date())
+                .setStatus("Pending")
                 .setTotalAmount(totalAmount)
                 .build();
     }
-
 
     public static Order createOrderWithStatus(int orderID, double totalAmount, String status) {
         return new Order.Builder()
