eda9cdc92ae761debe7a7b220af168e791ba0e19
Mkhisto <165008523+Mkhisto@users.noreply.github.com>
2025-10-19 21:31:17 +0200

    Update ContactController.java

--- src/main/java/za/ac/cput/controller/ContactController.java
+++ src/main/java/za/ac/cput/controller/ContactController.java
@@ -1,6 +1,7 @@
 package za.ac.cput.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import za.ac.cput.domain.Contact;
 import za.ac.cput.factory.ContactFactory;
@@ -9,7 +10,7 @@ import za.ac.cput.service.ContactService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/contact")
+@RequestMapping("/contacts")
 public class ContactController {
 
     private final ContactService service;
@@ -20,6 +21,7 @@ public class ContactController {
     }
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('ADMIN')")
     public Contact create(@RequestBody Contact contact) {
 
         Contact newContact = ContactFactory.createContact(contact.getEmail(), contact.getPhoneNumber());
@@ -27,21 +29,25 @@ public class ContactController {
     }
 
     @GetMapping("/read/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public Contact read(@PathVariable int id) {
         return service.read(id);
     }
 
     @PutMapping("/update")
+    @PreAuthorize("hasRole('ADMIN')")
     public Contact update(@RequestBody Contact contact) {
         return service.update(contact);
     }
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public boolean delete(@PathVariable int id) {
         return service.delete(id);
     }
 
     @GetMapping("/all")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<Contact> getAll() {
         return service.getAllContacts();
     }
