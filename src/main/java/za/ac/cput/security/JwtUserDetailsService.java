2aacabad5c888b3eb730c2da90d0f6d39d004b8f
Nontando Zondi <221248986@mycput.ac.za>
2025-10-21 13:02:32 +0200

    security

---
+++ src/main/java/za/ac/cput/security/JwtUserDetailsService.java
@@ -0,0 +1,46 @@
+package za.ac.cput.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.User;
+import za.ac.cput.service.IUserService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final IUserService userService;
+
+    @Autowired
+    public JwtUserDetailsService(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getContact().getEmail(),
+                user.getPassword(),
+                getAuthorities(user)
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+        return authorities;
+    }
+}
